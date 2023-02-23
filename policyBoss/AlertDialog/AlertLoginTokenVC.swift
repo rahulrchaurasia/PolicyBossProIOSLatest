e5a4cc031dcf756152e4f79e341b7f9a30153542
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2023-02-23 19:30:12 +0530

    first commit

---
+++ policyBoss/AlertDialog/AlertLoginTokenVC.swift
@@ -0,0 +1,30 @@
+//
+//  AlertLoginTokenVC.swift
+//  policyBoss
+//
+//  Created by Rahul Chaurasia on 23/02/23.
+//  Copyright © 2023 policyBoss. All rights reserved.
+//
+
+import UIKit
+
+class AlertLoginTokenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ policyBoss/Model/deviceDetail/OauthTokenModel.swift
@@ -0,0 +1,9 @@
+//
+//  OauthTokenModel.swift
+//  policyBoss
+//
+//  Created by Rahul Chaurasia on 23/02/23.
+//  Copyright © 2023 policyBoss. All rights reserved.
+//
+
+import Foundation
